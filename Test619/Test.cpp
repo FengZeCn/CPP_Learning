1d72c0f6a497fdd00d3365b24de96807e2f8adf3
FengZeCn <1183904840@qq.com>
2022-06-19 16:49:51 +0800

    _1

---
+++ Test619/Test.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2022-06-19 16:05:35
+ * @LastEditTime: 2022-06-19 16:07:21
+ * @LastEditors: LegionR9000P
+ * @Description: In User Settings Edit
+ * @FilePath: \CPP\Test619\Test.cpp
+ */
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"This is a test!!";
+    return 0;
+}
